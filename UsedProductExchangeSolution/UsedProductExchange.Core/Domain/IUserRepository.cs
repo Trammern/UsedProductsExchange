57e30024e00723deafb02612d8022a1296204a54
rizvan99 <rizvan2312@hotmail.com>
2020-11-24 10:44:22 +0100

    setting up interface and first test

--- UsedProductExchangeSolution/UsedProductExchange.Core/Domain/IUserRepository.cs
+++ UsedProductExchangeSolution/UsedProductExchange.Core/Domain/IUserRepository.cs
@@ -1,11 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using UsedProductExchange.Core.Entities;
 
 namespace UsedProductExchange.Core.Domain
 {
     public interface IUserRepository
     {
-        object Users { get; }
+
+        // CREATE
+        public User CreateUser(User user);
+
+        // READ
+        public IEnumerable<User> GetAllUsers();
+        public User GetUserById(int id);
+
+
+        // UPDATE
+        public User UpdateUser(User userToUpdate);
+
+
+        // DELETE
+        public User DeleteUser(int id);
+
     }
 }
--- UsedProductExchangeSolution/UsedProductExchange.XUnitTestProject/UserServiceTests.cs
+++ UsedProductExchangeSolution/UsedProductExchange.XUnitTestProject/UserServiceTests.cs
@@ -1,13 +1,46 @@
 using System;
 using Xunit;
+using Moq;
+using UsedProductExchange.Core.Domain;
+using System.Collections.Generic;
+using UsedProductExchange.Core.Entities;
+using UsedProductExchange.Core.Application.Implementation;
 
 namespace UsedProductExchange.XUnitTestProject
 {
     public class UserServiceTest
     {
-        [Fact]
-        public void Test1()
+        private List<User> users = null;
+        private readonly Mock<IUserRepository> repoMock;
+
+        public UserServiceTest()
         {
+            repoMock = new Mock<IUserRepository>();
+            repoMock.Setup(repo => repo.GetAllUsers()).Returns(() => users);
+        }
+
+        [Theory]
+        [InlineData(1, "Jimmy", "jimster", "qaz123", "Storegade 1", "jimster@hotmail.com", true)]
+        public void TestIfNewsUserIsCreated(int id, string name, string username, string password, string address, string email, bool role)
+        {
+            // ARRANGE
+            User user = new User()
+            {
+                UserId = id,
+                Name = name,
+                Username = username,
+                Password = password,
+                Address = address,
+                Email = email,
+                Role = role
+            };
+            UserService us = new UserService(repoMock.Object);
+
+            // ACT
+            var newUser = us.CreateUser(user);
+
+            // ASSERT
+            repoMock.Verify(repo => repo.CreateUser(user), Times.Once);
 
         }
     }
