64cfbdf39d0a9f65b322f76204d5ea72ecf0de18
Trammern <55829858+Trammern@users.noreply.github.com>
2020-11-26 13:07:34 +0100

    Try

--- UsedProductExchangeSolution/UsedProductExchange.Infrastructure/Repositories/ProductRepository.cs
+++ UsedProductExchangeSolution/UsedProductExchange.Infrastructure/Repositories/ProductRepository.cs
@@ -1,53 +1,61 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using UsedProductExchange.Core.Domain;
 using UsedProductExchange.Core.Entities;
-using UsedProductExchange.Infrastructure.Context;
 
 namespace UsedProductExchange.Infrastructure
 {
     public class ProductRepository : IRepository<Product>
     {
-
-        private readonly UsedProductExchangeContext _ctx;
-
-        public ProductRepository(UsedProductExchangeContext ctx)
-        {
-            _ctx = ctx;
-        }
-
         public Product Add(Product entity)
         {
-            _ctx.Attach(entity).State = EntityState.Added;
-            _ctx.SaveChanges();
-            return entity;
+            throw new NotImplementedException();
+        }
+
+        public Product CreateProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product DeleteProduct(Product product)
+        {
+            throw new NotImplementedException();
         }
 
         public Product Edit(Product entity)
         {
-            var productToUpdate = _ctx.Products.Update(entity);
-            _ctx.SaveChanges();
-            return productToUpdate.Entity;
+            throw new NotImplementedException();
         }
 
         public Product Get(int id)
         {
-            return _ctx.Products.FirstOrDefault(p => p.ProductId == id);
+            throw new NotImplementedException();
         }
 
         public IEnumerable<Product> GetAll()
         {
-            return _ctx.Products.ToList();
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GetProductById(int id)
+        {
+            throw new NotImplementedException();
         }
 
         public Product Remove(int id)
         {
-            var productToDelete = Get(id);
-            _ctx.Products.Remove(productToDelete);
-            return productToDelete;
+            throw new NotImplementedException();
+        }
+
+        public Product UpdateProduct(Product userToUpdate)
+        {
+            throw new NotImplementedException();
         }
     }
 }
