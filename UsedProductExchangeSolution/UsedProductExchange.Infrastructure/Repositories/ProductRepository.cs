4da28389715d225845b7d261cdbc1a78f9bd6bcb
rizvan99 <rizvan2312@hotmail.com>
2020-11-26 12:54:45 +0100

    Repository + controller for products

--- UsedProductExchangeSolution/UsedProductExchange.Infrastructure/Repositories/ProductRepository.cs
+++ UsedProductExchangeSolution/UsedProductExchange.Infrastructure/Repositories/ProductRepository.cs
@@ -1,36 +1,53 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using UsedProductExchange.Core.Domain;
 using UsedProductExchange.Core.Entities;
+using UsedProductExchange.Infrastructure.Context;
 
 namespace UsedProductExchange.Infrastructure
 {
     public class ProductRepository : IRepository<Product>
     {
+
+        private readonly UsedProductExchangeContext _ctx;
+
+        public ProductRepository(UsedProductExchangeContext ctx)
+        {
+            _ctx = ctx;
+        }
+
         public Product Add(Product entity)
         {
-            throw new NotImplementedException();
+            _ctx.Attach(entity).State = EntityState.Added;
+            _ctx.SaveChanges();
+            return entity;
         }
 
         public Product Edit(Product entity)
         {
-            throw new NotImplementedException();
+            var productToUpdate = _ctx.Products.Update(entity);
+            _ctx.SaveChanges();
+            return productToUpdate.Entity;
         }
 
         public Product Get(int id)
         {
-            throw new NotImplementedException();
+            return _ctx.Products.FirstOrDefault(p => p.ProductId == id);
         }
 
         public IEnumerable<Product> GetAll()
         {
-            throw new NotImplementedException();
+            return _ctx.Products.ToList();
         }
 
         public Product Remove(int id)
         {
-            throw new NotImplementedException();
+            var productToDelete = Get(id);
+            _ctx.Products.Remove(productToDelete);
+            return productToDelete;
         }
     }
 }
