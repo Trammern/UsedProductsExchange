23b7ef56abe47ae1353ade38e4b00ebfd3c8b791
Trammern <55829858+Trammern@users.noreply.github.com>
2020-11-26 12:42:11 +0100

    DBInitializer

---
+++ UsedProductExchangeSolution/UsedProductExchange.Infrastructure/DBInitializer/DBInitializer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UsedProductExchange.Core.Domain;
+using UsedProductExchange.Infrastructure.Context;
+
+namespace UsedProductExchange.Infrastructure.DBInitializer
+{
+   public class DBInitializer
+    {
+        
+        
+        private readonly IRepository _repository;
+        private readonly UsedProductExchangeContext _ctx;
+
+        public DBInitializer(
+                 UsedProductExchangeContext ctx,
+                
+            _ctx = ctx;
+            _customerRepository = customerRepository;
+            _addressRepository = addressRepository;
+            _cityRepository = cityRepository;
+            _countryRepository = countryRepository;
+        }
+    }
+}
