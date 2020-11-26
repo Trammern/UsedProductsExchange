587fe00c805b74532e802d99e4404852c59954a9
math231k <54638811+math231k@users.noreply.github.com>
2020-11-26 11:35:49 +0100

    Feature complete
    Implementet all controller methods

--- UsedProductExchangeSolution/UsedProductExchange.UI/Controllers/UserController.cs
+++ UsedProductExchangeSolution/UsedProductExchange.UI/Controllers/UserController.cs
@@ -2,6 +2,8 @@
 using UsedProductExchange.Core.Application;
 using UsedProductExchange.Core.Entities;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System;
 
 namespace UsedProductExchange.UI.Controllers
 {
@@ -16,9 +18,63 @@ namespace UsedProductExchange.UI.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<User> Get()
+        public ActionResult<IEnumerable<User>> Get()
         {
-            return _iUserService.GetAllUsers();
+            var userList = _iUserService.GetAllUsers().ToList();
+            if (userList.Count == 0)
+            {
+                return NoContent();
+            }
+            return Ok(userList);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<User> GetById(int id)
+        {
+            var result = _iUserService.GetUserById(id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult<User> Delete(int id)
+        {
+            var result = _iUserService.DeleteUser(id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return result;
+        }
+        [HttpPost]
+        public ActionResult<User> Post([FromBody] User obj)
+        {
+            try
+            {
+                var result = _iUserService.CreateUser(obj);
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] User obj)
+        {
+            try
+            {
+                var result = _iUserService.UpdateUser(obj);
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
         }
     }
 }
