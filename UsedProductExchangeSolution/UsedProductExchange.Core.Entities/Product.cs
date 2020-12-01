d1c241a9e71d862cc8763318b66349b7144dcfb5
rizvan99 <rizvan2312@hotmail.com>
2020-12-01 08:26:29 +0100

    hotfix

--- UsedProductExchangeSolution/UsedProductExchange.Core.Entities/Product.cs
+++ UsedProductExchangeSolution/UsedProductExchange.Core.Entities/Product.cs
@@ -14,5 +14,6 @@ namespace UsedProductExchange.Core.Entities
         public double CurrentPrice { get; set; }
         public DateTime Expiration { get; set; }
         public int CategoryId { get; set; }
+        public Category Category { get; set; }
     }
 }
