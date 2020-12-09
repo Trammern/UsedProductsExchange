4c5268c40b83d90550535292cd829352d48f0516
math231k <54638811+math231k@users.noreply.github.com>
2020-12-09 13:40:06 +0100

    Getting ready for edit user functionality

--- UsedProductsExchangeFrontend/src/app/profiles/edit-profile/edit-profile.component.ts
+++ UsedProductsExchangeFrontend/src/app/profiles/edit-profile/edit-profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from '../../_models/user';
+import {AuthenticationService} from '../../_services/authentication.service';
 
 @Component({
   selector: 'app-edit-profile',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-profile.component.css']
 })
 export class EditProfileComponent implements OnInit {
+  user: User;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private authenticationService: AuthenticationService) {
+    authenticationService.user.subscribe(user => this.setUser(user));
   }
 
+  ngOnInit(): void {
+    this.user = this.authenticationService.getUser();
+  }
+
+  setUser(user: User): void {
+    this.user = user;
+  }
 }
